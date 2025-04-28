5d549fb7159e95d4de47b14b07db83e8d5dffd7b
PavelAC <pavelandreicalin@gmail.com>
2025-04-28 14:56:33 +0300

    preaparations

---
+++ src/app/pages/craving-help/craving-help.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-craving-help',
+  standalone: true,
+  imports: [],
+  templateUrl: './craving-help.component.html',
+  styleUrl: './craving-help.component.scss'
+})
+export class CravingHelpComponent {
+
+}
